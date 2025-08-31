ab21d4c66c04956c2e26d1d0cdac3ae47383d094
VICEROY\Camryn <camrynschroder@gmail.com>
2025-08-30 21:37:37 -0400

    Background now renders to a RenderTexture for future effects

---
+++ Assets/Scripts/Core/FullscreenRenderTexture.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Camera))]
+public class FullscreenRenderTexture : MonoBehaviour
+{
+    private Camera renderCamera;
+    private RenderTexture renderTexture;
+
+    private void Awake()
+    {
+        renderCamera = GetComponent<Camera>();
+
+        if (renderCamera.targetTexture)
+        {
+            renderTexture = renderCamera.targetTexture;
+        }
+        else
+        {
+            renderTexture = new RenderTexture(Screen.width, Screen.height, 16);
+        }
+    }
+
+    private void OnDisable()
+    {
+        renderTexture.Release();
+    }
+
+    private void Update()
+    {
+        renderTexture.Release();
+        renderTexture.width = Screen.width;
+        renderTexture.height = Screen.height;
+        renderCamera.ResetAspect();
+    }
+}
